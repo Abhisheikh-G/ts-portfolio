cbe41603c10380ed5e1f0a38da88f1ec7f7cca85
Abhisheikh Gill <abhisheikhg@gmail.com>
2021-10-20 21:05:54 -0400

    Add github link in header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -2,15 +2,15 @@ import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import Image from "next/image";
 import Drawer from "../Drawer/Drawer";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import Link from "../../src/Link";
 import { useTheme } from "@mui/material";
 
-export default function ButtonAppBar() {
+export default function Header() {
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
   return (
@@ -30,7 +30,7 @@ export default function ButtonAppBar() {
             aria-label="menu"
             onClick={() => setOpen(!open)}
           >
-            <MenuIcon />
+            <MenuIcon fontSize="large" />
           </IconButton>
           <Drawer open={open} setOpen={setOpen} />
 
@@ -43,7 +43,15 @@ export default function ButtonAppBar() {
               width={100}
             />
           </Box>
-          <Box height={25} width={25} />
+          <Box>
+            <Link
+              href="https://github.com/Abhisheikh-G"
+              target="_blank"
+              sx={{ color: "white" }}
+            >
+              <GitHubIcon fontSize="large" />
+            </Link>
+          </Box>
         </Toolbar>
       </AppBar>
     </Box>
