a7484e927a58c8665cb78ab38f600c862d1bf27d
Abhisheikh Gill <abhisheikhg@gmail.com>
2021-10-25 14:35:12 -0400

    Add font preload to stop FOUT

--- public/sw.js
+++ public/sw.js
@@ -81,7 +81,7 @@ if (!self.define) {
     });
   };
 }
-define("./sw.js",['./workbox-f88dbe3b'], function (workbox) { 'use strict';
+define("./sw.js",['./workbox-1ffba242'], (function (workbox) { 'use strict';
 
   /**
   * Welcome to your Workbox-powered service worker!
@@ -96,12 +96,33 @@ define("./sw.js",['./workbox-f88dbe3b'], function (workbox) { 'use strict';
   */
 
   importScripts();
-  workbox.skipWaiting();
+  self.skipWaiting();
   workbox.clientsClaim();
+  workbox.registerRoute("/", new workbox.NetworkFirst({
+    "cacheName": "start-url",
+    plugins: [{
+      cacheWillUpdate: async ({
+        request,
+        response,
+        event,
+        state
+      }) => {
+        if (response && response.type === 'opaqueredirect') {
+          return new Response(response.body, {
+            status: 200,
+            statusText: 'OK',
+            headers: response.headers
+          });
+        }
+
+        return response;
+      }
+    }]
+  }), 'GET');
   workbox.registerRoute(/.*/i, new workbox.NetworkOnly({
     "cacheName": "dev",
     plugins: []
   }), 'GET');
 
-});
+}));
 //# sourceMappingURL=sw.js.map
