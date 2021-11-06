3b465ac21d765b9b4c0e6048ea758fb227a6d616
Abhisheikh Gill <abhisheikhg@gmail.com>
2021-11-06 18:36:04 -0400

    Change underline to three lines

--- components/Underline/Underline.tsx
+++ components/Underline/Underline.tsx
@@ -5,14 +5,33 @@ const Underline = () => {
   const theme = useTheme();
   // const classes = useStyles();
   return (
-    <Box
-      sx={{
-        width: "inherit",
-        height: 5,
-        borderRadius: 20,
-        background: `linear-gradient(to right, ${theme.palette.custom.yellow},${theme.palette.secondary.main})`,
-      }}
-    />
+    <>
+      <Box
+        sx={{
+          width: "inherit",
+          height: 2,
+
+          background: `linear-gradient(to right, ${theme.palette.custom.yellow},${theme.palette.secondary.main})`,
+        }}
+      />
+      <Box
+        sx={{
+          width: "inherit",
+          height: 2,
+          my: "1px",
+
+          background: `linear-gradient(to right, ${theme.palette.custom.yellow},${theme.palette.secondary.main})`,
+        }}
+      />
+      <Box
+        sx={{
+          width: "inherit",
+          height: 2,
+
+          background: `linear-gradient(to right, ${theme.palette.custom.yellow},${theme.palette.secondary.main})`,
+        }}
+      />
+    </>
   );
 };
 
