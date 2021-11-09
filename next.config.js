7bc7e09def511317a8ecdb744793d2cac33fdfa6
Abhisheikh Gill <abhisheikhg@gmail.com>
2021-11-09 17:23:42 -0500

    Change next.config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withPWA({
   pwa: {
     dest: "public",
     disable: process.env.NODE_ENV === "development",
+    runtimeCaching,
+    buildExcludes: [/middleware-manifest\.json$/],
   },
 });
